3b853af6759ffe3dac291c757e0a55df2bec7545
wo8ow <alejandro.ortizmend@gmail.com>
2026-06-29 12:01:31 -0500

    cambios

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -24,19 +24,36 @@ export class MenuPage implements OnInit {
     this.servicio.getSession('idpersona').then((res:any) => {
       this.id_persona = res;
       this.dpersona(this.id_persona);
+      this.lcontactos(this.id_persona);
     })
   }
   ngOnInit() {}
+
   dpersona(id_persona: string) {
     let datos = {
       accion: 'consulta', // ⚠️ Verifica en tu persona.php cómo se llama exactamente esta acción ('consulta', 'datos', etc.)
-      codigo: id_persona  // ⚠️ Verifica cómo espera recibir PHP tu ID
+      cod_persona: id_persona  // ⚠️ Verifica cómo espera recibir PHP tu ID
     };
     this.servicio.sendData(datos, 'persona').subscribe((res: any) => {
       if(res.estado){
-        this.datospersona = res.datos;
+        this.datospersona = res.persona;
         this.nombre = this.datospersona.nombre + " " + this.datospersona.apellido;
       }
     });
+  }
+
+  lcontactos(id_persona: string) {
+    let datos = {
+      accion: 'consulta', 
+      cod_persona: id_persona
+    };
+    this.servicio.sendData(datos, 'contacto').subscribe((res: any) => {
+      if(res.estado){
+        this.contactos=res.contactos
+      }
+      else{
+        this.servicio.showToast(res.mensaje,3000)
+      }
+    })
   }
 }
